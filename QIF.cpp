d2b3c524ef7a329d695a12150a9d520830cc2ff8
igor <igor.s.lemes@gmail.com>
2018-08-31 05:17:53 -0300

    function isChannel and   some comments

--- QIF.cpp
+++ QIF.cpp
@@ -13,12 +13,34 @@
 // Definindo um tipo de dado para a imprimir a precisão
 typedef std::numeric_limits< double > dbl;
 // Definindo um tipo de dado de matriz
-boost::numeric::ublas::matrix<long double>  matriz;
+boost::numeric::ublas::matrix< long double >  MATRIZ;
+// Definindo um tipo de dado de vetor
+std::vector< long double > VETOR;
 
 
+ 
+bool isChannel( MATRIZ m ){
+    long double totalSum = 0;
+    int rows = 0;
+    for(size_t i = 0; i < m.size1 (); i++){
+        totalSum = 0;
+        for (size_t i = 0; i < m.size2(); i++){
+            totalSum += m (i, j);
+            printf("Iterate\n");
+        }
+        if (totalSum <= (long double) 1.0000000000000001 and totalSum >= (long double) 0.9999999999999999){
+            rows += 1;
+        } else {
+            return false;
+        }
+    }
+    if(rows == m.size1()){
+        return true;
+    }
+}
 
 
-bool isDistribution(std::vector<long double> numberVector){
+bool isDistribution( VETOR numberVector){
     long double totalSum = 0;
     for (size_t i = 0; i < numberVector.size(); i++){
         totalSum += numberVector[i];
@@ -32,7 +54,7 @@ bool isDistribution(std::vector<long double> numberVector){
 }
 
 int main(){
-    std::vector<long double> probability = {1.0/3.0, 1.0/3.0, 1.0/3.0};
+    VETOR probability = {1.0/3.0, 1.0/3.0, 1.0/3.0};
     
     std::cout.precision(dbl::max_digits10);
     
